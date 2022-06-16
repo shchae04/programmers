8beaca92ee463fb2ff32250dbd6bc2251d408f89
shchae04 <shchae04@naver.com>
2022-06-16 16:05:24 +0900

    programmers JsonParserV2 start at Home

--- demo/demo/src/main/java/com/example/demo/JsonParserV2.java
+++ demo/demo/src/main/java/com/example/demo/JsonParserV2.java
@@ -1,7 +1,7 @@
 package com.example.demo;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class JsonParserV2 {
 
@@ -41,33 +41,95 @@ public class JsonParserV2 {
         this.data = data;
     }
 
+    /*
+    parsing() 함수는 JsonParserV2를 받아서 data의 공백을 제거하고 char단위로 나누어서 리스트에 담아주는 역할을 하는 함수.
+     */
     public JsonParserV2 parsing() {
-        List<StringBuilder> list = new ArrayList<>();
-        String kd = "";
-        for(int i=0; i<data.length(); i++){
-            StringBuilder sbd = new StringBuilder();
+        //trim();
+        this.data = data.replace(" ", "");
 
-            sbd.append(data.charAt(i));
+        //char 단위로 나누어서 List에 담기
+        List<Character> list = new ArrayList<>();
 
-            list.add(sbd);
-            kd += list.get(i);
+        for (int i = 0; i < data.length(); i++) {
+            list.add(data.charAt(i));
         }
-        System.out.println(kd.replace(" ",""));
+        System.out.println(list);
+
+
+        // 1. 호출
+        tokenizer(list);
+
+        return this;
+    }
+
+    // Character가 담겨있는 리스트를 받아서 delimeter 의 개수를 카운트 해주는 함수 tokenizer()
+    public JsonParserV2 tokenizer(List<Character> list) {
+
+        //JSON data 에 있는 delimter.
+        char sBrace = '{'; // 중괄호 시작
+        char eBrace = '}'; // 중괄호 끝
+        char sBraket = '['; // 대괄호 시작
+        char eBraket = ']'; // 대괄호 끝
+        char dot = '\"'; // " 시작
+
+        int countSbrace = 0;
+        int countEbrace = 0;
+        int countSbraket = 0;
+        int countEbraket = 0;
+        int countDOt = 0;
+
+        //현재 문자열의 인덱스 위치를 나타냄 위치판단의 지표
+        int curIdx = 0;
+
+        for (curIdx = 0; curIdx < list.size(); curIdx++) {
+            if (list.get(curIdx).equals(sBrace)) {
+                countSbrace++;
+                String token = "";
+                token += list.get(curIdx).toString();
+            }
+            if (list.get(curIdx).equals(eBrace)) {
+                countEbrace++;
+            }
+            if (list.get(curIdx).equals(sBraket)) {
+                countSbraket++;
+            }
+            if (list.get(curIdx).equals(eBraket)) {
+                countEbraket++;
+            }if (list.get(curIdx).equals(dot)) {
+                countDOt++;
+            }
+        }
+
+        System.out.println("중괄호 시작 개수 :" + countSbrace);
+        System.out.println("중괄호 끝 개수 :" + countEbrace);
+        System.out.println("대괄호 시작 개수 :" + countSbraket);
+        System.out.println("대괄호 끝 개수 :" + countEbraket);
+        System.out.println("'\"' 개수" + countDOt);
+        //4, 4, 1, 1 개수 끝까지 파싱해보면 된다.
+
+        // { 기호를 가지고 나왔을 경우 먹어치운다. [ 기호가 나오면 cnt + 1을 하고 ] 기호가 나오면 cnt - 1처리 하고 그중간 값을 리턴.
+        // } 기호 까지 묶어서 리스트에 담아서 리턴합니다.
+
+        String getToken = "";
+        getToken += list.get(curIdx-1);
+
         return this;
     }
 
 
-    // list를 받아서 for문을 통해서 delimeter를 세고 토큰 단위로 리스트에 집어 넣는다 전부 잘라서 char화 시키면 된다.
+    // 토큰화 하는 함수 tokenizing
+    public JsonParserV2 getResult() {
 
-    //리스트에 담으면 된다.
+        return this;
+    }
+
 
     /*
         중괄호로 Json데이터 시작 "data" 라는 이름으로 배열이 시작 [ {},{},{} ] 중괄호로 데이터 끝. 파싱해라.
         개수를 세야 할듯 {의 개수와 }의 개수가 동일해야 합니다. [이 있다면 끝까지 탐색후 [ 전에 있는 데이터의 이름으로 배열 을 설정합니다.
 
      */
-
-
 
 
 }
