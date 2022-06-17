c6f457500c82415ce77cfce012f291edd6f04323
shchae04 <shchae04@naver.com>
2022-06-17 16:39:26 +0900

    programmers JsonParserV2 start at Home

--- demo/demo/src/main/java/com/example/demo/JsonParserV2.java
+++ demo/demo/src/main/java/com/example/demo/JsonParserV2.java
@@ -1,7 +1,9 @@
 package com.example.demo;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
+import java.util.Map;
 
 public class JsonParserV2 {
 
@@ -9,6 +11,7 @@ public class JsonParserV2 {
         기존 JsonParser같은 경우에는 JSON data안에 객체 안에 객체가 있는 경우에는 파싱이 불가능했다.
         이러한점을 보완하고자 JsonParserV2를 만들어보자.
      */
+
 
     public static void main(String[] args) {
         String jsondata = "{\n" +
@@ -46,7 +49,7 @@ public class JsonParserV2 {
      */
     public JsonParserV2 parsing() {
         //trim();
-        this.data = data.replace(" ", "");
+        this.data = data.replace(" ", "").replace("\n","");
 
         //char 단위로 나누어서 List에 담기
         List<Character> list = new ArrayList<>();
@@ -82,24 +85,40 @@ public class JsonParserV2 {
         //현재 문자열의 인덱스 위치를 나타냄 위치판단의 지표
         int curIdx = 0;
 
+        int braceCount = 0;
+
+        int braketCount = 0;
+
+            String JsonArr = "";
         for (curIdx = 0; curIdx < list.size(); curIdx++) {
+            JsonArr += list.get(curIdx);
+            // { 를 만난 경우
             if (list.get(curIdx).equals(sBrace)) {
                 countSbrace++;
-                String token = "";
-                token += list.get(curIdx).toString();
+                braceCount++;
             }
+            // } 를 만난 경우
             if (list.get(curIdx).equals(eBrace)) {
                 countEbrace++;
+                braceCount--;
             }
+            // [ 를 만난 경우
             if (list.get(curIdx).equals(sBraket)) {
                 countSbraket++;
+                braketCount++;
             }
+            // ] 를 만난 경우
             if (list.get(curIdx).equals(eBraket)) {
                 countEbraket++;
-            }if (list.get(curIdx).equals(dot)) {
+                braketCount--;
+                // " 를 만난 경우
+            }
+            if (list.get(curIdx).equals(dot)) {
                 countDOt++;
             }
         }
+
+        // 개수를 세면서 잘라 줄 함수.
 
         System.out.println("중괄호 시작 개수 :" + countSbrace);
         System.out.println("중괄호 끝 개수 :" + countEbrace);
@@ -110,16 +129,32 @@ public class JsonParserV2 {
 
         // { 기호를 가지고 나왔을 경우 먹어치운다. [ 기호가 나오면 cnt + 1을 하고 ] 기호가 나오면 cnt - 1처리 하고 그중간 값을 리턴.
         // } 기호 까지 묶어서 리스트에 담아서 리턴합니다.
+        List<String> jsonList = new ArrayList<>();
 
-        String getToken = "";
-        getToken += list.get(curIdx-1);
+        // {"data":[{"id":1,"name":"공유","address":"서울"},{"id":2,"name":"박서준","address":"부산"},{"id":3,"name":"수박","address":"제주도"}]}
+        System.out.println(JsonArr);
+        data = JsonArr;
+
+        byteTokenizer(list);
 
         return this;
     }
 
 
-    // 토큰화 하는 함수 tokenizing
-    public JsonParserV2 getResult() {
+    // char값을 받아서 토큰화 하는 함수 tokenizing
+    public JsonParserV2 byteTokenizer(List<Character> list) {
+
+        char pivot; //기준값으로 설정. 비교할 값! 리스트에서 비교할 거라서 따로 생각할 필요는 없을듯.
+        //시작값
+        int begin = 0;
+        //끝값
+        int end = list.size();
+
+        while (begin < end) {
+
+        }
+
+
 
         return this;
     }
